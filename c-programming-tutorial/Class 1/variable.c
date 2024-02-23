478be7f959885a21b9f36d5047373a77fb7533a9
Mahfuz Hasan Reza <mahfuzhasanreza1@gmail.com>
2024-02-24 02:37:19 +0600

    add class 1 and 2 code

---
+++ c-programming-tutorial/Class 1/variable.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+	int number;
+	number=10;
+
+	int number2=11;
+
+	float num3=1.2;
+
+	number2=12;
+
+	printf("%d", number2);
+	
+
+	return 0;
+}
---
+++ c-programming-tutorial/Class 2/basic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+	int number1 = 8.999;
+	float number2 = 11;
+	long int number3 = 1111111111111;
+	char ch = '*';
+	
+	printf("%d %f %ld %c", number1, number2, number3, ch);
+	
+	return 0;
+}
